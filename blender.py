b5f6495f2a5cd5ac74f7568326c40354ddc594da
jackhasakeyboard <jackhasakeyboard@gmail.com>
2018-11-13 20:42:32 -0700

    asdf

--- blender.py
+++ blender.py
@@ -4,34 +4,52 @@ from bpy.types import Panel
 import os
 import re
 
-# class for panel
+# commit menu
+"""
+class Commit(bpy.types.Menu):
+    bl_idname = "kit_commit"
+    bl_label = "SHA"
+
+    def draw(self, context):
+        for asdf in ["asdf", "hjkl"]:
+            self.layout.operator("object.select_all", text = asdf).action = "TOGGLE"
+"""
+
 class Kit(Panel):
     bl_space_type = "VIEW_3D"
     bl_region_type = "TOOLS"
-    bl_label = "History: "
+    bl_label = "test"
     bl_context = "objectmode"
     bl_category = "Kit"
 
     def draw(self, ctx):
-        layout = self.layout
+        commit = open(os.getcwd() + "/kit/test/.git/logs/refs/heads/master", "r", encoding="utf-8")
+        l = []
+        for _ in commit:
+            l.append(_)
 
-        # obj
-        f = os.listdir(os.getcwd() + "/test")
+        commit.close()
 
-        regexObj = re.compile(".+\.obj")
-        obj = list(filter(regexObj.match, f))
+        msg = []
+        for _ in l:
+            msg.append(re.search(r"(?<=commit).+", _).group(0))\
+
+
+        self.layout.prop(ctx.scene, "commit")
+
+        # message
+        self.layout.label(text = msg[0])
+
 
         """
-        for f in obj:
-            layout.operator("mesh.primitive_cube_add", text=f)
-        """
-
         # branch
-        branch = os.listdir(os.getcwd() + "/test/.git/refs/heads")
+        branch = os.listdir(os.getcwd() + "/kit/test/.git/refs/heads")
 
         # commit
+        self.layout.menu("kit_commit")
+
         line = []
-        commit = open(os.getcwd() + "/test/.git/logs/refs/heads/master", "r", encoding="utf-8")
+        commit = open(os.getcwd() + "/kit/test/.git/logs/refs/heads/master", "r", encoding="utf-8")
         for c in commit:
             line.append(c)
 
@@ -44,16 +62,55 @@ class Kit(Panel):
             trunc = fst[0:5]
 
             msg = re.search(r"(?<=commit).+", l).group(0)
-            print(msg)
-            print("asdf")
 
-            layout.operator("mesh.primitive_cube_add", text=trunc + " " + msg)
+            self.layout.label(text = trunc + " " + msg)
+
+        # change
+        if not "asdf" in bpy.data.objects:
+            bpy.ops.import_scene.obj(filepath="./test/test.obj")
+
+            print(bpy.context.scene.objects.active.name)
+
+        else:
+            print("this it it chief")
+        """
+
 
 def register():
     bpy.utils.register_class(Kit)
 
+    # commit
+    commit = open(os.getcwd() + "/kit/test/.git/logs/refs/heads/master", "r", encoding="utf-8")
+    l = []
+    for _ in commit:
+        l.append(_)
+
+    commit.close()
+
+    sha = []
+    msg = []
+    for _ in l:
+        word = _.split(" ")
+
+        fst = word[0]
+        trunc = fst[0:5]
+
+        sha.append(trunc)
+        msg.append(re.search(r"(?<=commit).+", _).group(0))
+
+    tup = []
+    for _ in sha:
+        tup.append(("asdf", _, "asdf"))
+
+    bpy.types.Scene.commit = bpy.props.EnumProperty(
+        name = "Commit",
+        items = tup
+    )
+
 def unregister():
+    #bpy.utils.unregister_claKitKit)
     bpy.utils.unregister_class(Kit)
+    #bpy.utils.unregister_class(Commit)
 
 if __name__ == "__main__":
     register()
